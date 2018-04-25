a7cf0c6c8f24267f16233cf09d34e537b6d140c4
regzand <regzand@gmail.com>
2018-04-25 10:10:38 +0200

    Changed the way tokens are handled

--- tokens.py
+++ tokens.py
@@ -1,22 +1,50 @@
 from enum import Enum
+from typing import List
+import math
+import string
 
 
 class TokenType(Enum):
-    UNKNOWN = 0
-    IDENTIFIER = 1
-    CONSTANT = 2
-    DOUBLE_OPERATOR = 3
-    SINGLE_OPERATOR = 4
-    OPENING_BRACKET = 5
-    CLOSING_BRACKET = 6
+    OPENING_BRACKET = (1,           "(")
+    CLOSING_BRACKET = (1,           ")")
+    SINGLE_OPERATOR = (1,           "~")
+    DOUBLE_OPERATOR = (1,           "|&^>=")
+    CONSTANT        = (1,           "01")
+    IDENTIFIER      = (math.inf,    string.ascii_letters + string.digits)
+    WHITESPACE      = (math.inf,    string.whitespace)
+    UNKNOWN         = (1,           "")
+
+    def __init__(self, max_length: int, chars: str):
+        self.max_length = max_length
+        self.chars = chars
 
 
 class Token(str):
 
-    def __new__(cls, value: str, position: int, type: TokenType):
+    def __new__(cls, value: str, type: TokenType, position: int = 0):
         return super(Token, cls).__new__(cls, value)
 
-    def __init__(self, value:str , position: int, type: TokenType):
+    def __init__(self, value:str, type: TokenType, position: int = 0):
         super().__init__()
         self.position = position
         self.type = type
+
+
+def get_token(exp: str) -> Token:
+    length = 0
+    for type in TokenType:
+        while length < len(exp) and length < type.max_length and exp[length] in type.chars:
+            length += 1
+        if length > 0:
+            return Token(exp[:length], type)
+    return Token(exp[0], TokenType.UNKNOWN)
+
+
+def tokenize(exp: str) -> List[Token]:
+    tokens = []
+    i = 0
+    while i < len(exp):
+        tokens.append(get_token(exp[i:]))
+        tokens[-1].position = i
+        i += len(tokens[-1])
+    return tokens
